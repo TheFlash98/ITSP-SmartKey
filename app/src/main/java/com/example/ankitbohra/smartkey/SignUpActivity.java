7d21c1c2788731adbe69c2ff48299a1732ea84d8
TheFlash98 <mastermind.sarthak@gmail.com>
2017-05-24 14:25:46 +0530

    POST request for login

--- app/src/main/java/com/example/ankitbohra/smartkey/SignUpActivity.java
+++ app/src/main/java/com/example/ankitbohra/smartkey/SignUpActivity.java
@@ -1,10 +1,20 @@
 package com.example.ankitbohra.smartkey;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+
+import java.io.IOException;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -12,27 +22,72 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
-    public void action(View view){
+
+
+    private static final String url = "http://192.168.1.4:5000/signup?";
+    private final OkHttpClient client = new OkHttpClient();
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    public void action(View view)throws IOException {
         EditText enterName = (EditText) findViewById(R.id.name);
+
         EditText password = (EditText) findViewById(R.id.password);
-        EditText confirm_password = (EditText)findViewById(R.id.confirm_password);
-        TextView warning = (TextView)findViewById(R.id.warning);
+
+        EditText confirm_password = (EditText) findViewById(R.id.confirm_password);
+
+        TextView warning = (TextView) findViewById(R.id.warning);
+
         String string = enterName.getText().toString();
-        if(!checkUniqueness(string)){
+
+        if (!checkUniqueness(string)) {
             warning.setText("Choose a different username");
             return;
         }
-
-        if(password.getText().toString().equals(confirm_password.getText().toString())){
+        String user_name = enterName.getText().toString();
+        String pass = password.getText().toString();
+        String conf_pass = confirm_password.getText().toString();
+        if (pass.equals(conf_pass)){
             warning.setText("Successful!");
+            RequestBody body = RequestBody.create(JSON, jsonMaker(user_name,pass));
+            Request request = new com.squareup.okhttp.Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(com.squareup.okhttp.Request request, IOException throwable) {
+                    throwable.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(com.squareup.okhttp.Response response) throws IOException {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                }
+            });
         }
-        else{
+        else {
             warning.setText("Passwords don't match. Recheck!");
+
         }
-        return;
     }
+
+    public String jsonMaker(String username, String password){
+        String json = "{\"email\":\"demo@gmail.com\","
+                + "\"username\":\""+username+"\","
+                + "\"phone\":\"123456789\","
+                + "\"password\":\""+password+"\""
+                + "}";
+
+        return json;
+    }
+
 
     public boolean checkUniqueness(String s){
         return true;
