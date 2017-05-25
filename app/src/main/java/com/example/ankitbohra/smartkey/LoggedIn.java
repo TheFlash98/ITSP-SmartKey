be89e543e13c0f9de0e7f8a78639e36a7b6a1e38
ankit110998 <ankit110998@gmail.com>
2017-05-25 23:15:38 +0530

    admin n user login page

--- app/src/main/java/com/example/ankitbohra/smartkey/LoggedIn.java
+++ app/src/main/java/com/example/ankitbohra/smartkey/LoggedIn.java
@@ -8,6 +8,6 @@ public class LoggedIn extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logged_in);
+        setContentView(R.layout.activity_adminlogin);
     }
 }
